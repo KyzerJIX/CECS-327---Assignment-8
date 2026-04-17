e7c457defd8c9bbf53fe9d953c5aff26b0d1703f
Truong Bui-Phan <hunte2999@gmail.com>
2026-04-17 15:03:53 -0700

    Enhance echo server for multiple client connections
    Refactor echo server to handle multiple clients and improve socket options.

--- echo_server.py
+++ echo_server.py
@@ -3,25 +3,29 @@ import socket
 port = int(input("Enter port: "))
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server_socket.bind(('', port))
-server_socket.listen(1)
+server_socket.listen(5)
 
 print("Server listening...")
 
-client_socket, client_address = server_socket.accept()
-print("Connected to", client_address)
-
 while True:
-    data = client_socket.recv(1024)
+    client_socket, client_address = server_socket.accept()
+    print("Connected to", client_address)
 
-    if not data:
-        break
+    while True:
+        data = client_socket.recv(1024)
 
-    message = data.decode()
-    print("Received:", message)
+        if not data:
+            print("Client disconnected:", client_address)
+            break
 
-    response = message.upper()
-    client_socket.send(response.encode())
+        message = data.decode()
+        print("Received:", message)
 
-client_socket.close()
-server_socket.close()
+        response = message.upper()
+        client_socket.sendall(response.encode())
+
+    client_socket.close()
